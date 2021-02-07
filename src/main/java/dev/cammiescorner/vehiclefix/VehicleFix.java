e578ede072e37928fa20aeb6183ea52d9df19239
CammiePone <inazuma261@gmail.com>
2021-02-07 13:50:09 -0500

    e

---
+++ src/main/java/dev/cammiescorner/vehiclefix/VehicleFix.java
@@ -0,0 +1,14 @@
+package dev.cammiescorner.vehiclefix;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VehicleFix implements ModInitializer
+{
+	public static final String MOD_ID = "vehiclefix";
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/dev/cammiescorner/vehiclefix/mixin/EntityMixin.java
@@ -0,0 +1,51 @@
+package dev.cammiescorner.vehiclefix.mixin;
+
+import dev.cammiescorner.vehiclefix.VehicleFix;
+import net.fabricmc.fabric.api.tag.TagRegistry;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin
+{
+	@Shadow	private Box entityBounds;
+	@Shadow @Final private EntityType<?> type;
+
+	@Shadow	public abstract boolean hasPassengers();
+	@Shadow	public abstract List<Entity> getPassengerList();
+
+	private static final Tag<EntityType<?>> AFFECTS = TagRegistry.entityType(new Identifier(VehicleFix.MOD_ID, "fixed_collision"));
+
+	@Redirect(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getBoundingBox()Lnet/minecraft/util/math/Box;"))
+	Box getBoundingBox(Entity entity)
+	{
+		Box box = entityBounds;
+
+		if(hasPassengers() && (AFFECTS.values().isEmpty() || AFFECTS.contains(type)))
+		{
+			for(Entity passenger : getPassengerList())
+			{
+				box = new Box(Math.min(box.minX, passenger.getBoundingBox().minX), box.minY, Math.min(box.minZ, passenger.getBoundingBox().minZ), Math.max(box.maxX, passenger.getBoundingBox().maxX), Math.max(box.maxY, passenger.getBoundingBox().maxY), Math.max(box.maxZ, passenger.getBoundingBox().maxZ));
+			}
+		}
+
+		return box;
+	}
+}
