3deac3e8d9966d27b54b33d789e24ec5f5f721b6
CammiePone <inazuma261@gmail.com>
2021-02-15 15:08:18 -0500

    f

--- src/main/java/dev/cammiescorner/vehiclefix/mixin/EntityMixin.java
+++ src/main/java/dev/cammiescorner/vehiclefix/mixin/EntityMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.World;
@@ -17,6 +18,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -25,19 +27,16 @@ import java.util.List;
 @Mixin(Entity.class)
 public abstract class EntityMixin
 {
-	@Shadow	private Box entityBounds;
 	@Shadow @Final private EntityType<?> type;
 
-	@Shadow	public abstract boolean hasPassengers();
-	@Shadow	public abstract List<Entity> getPassengerList();
+	@Shadow public abstract boolean hasPassengers();
+	@Shadow public abstract List<Entity> getPassengerList();
 
 	private static final Tag<EntityType<?>> AFFECTS = TagRegistry.entityType(new Identifier(VehicleFix.MOD_ID, "fixed_collision"));
 
-	@Redirect(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getBoundingBox()Lnet/minecraft/util/math/Box;"))
-	Box getBoundingBox(Entity entity)
+	@Inject(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/entity/Entity;getBoundingBox()Lnet/minecraft/util/math/Box;"), locals = LocalCapture.CAPTURE_FAILSOFT)
+	private void adjustMovementForCollisions(Vec3d movement, CallbackInfoReturnable<Vec3d> info, Box box)
 	{
-		Box box = entityBounds;
-
 		if(hasPassengers() && (AFFECTS.values().isEmpty() || AFFECTS.contains(type)))
 		{
 			for(Entity passenger : getPassengerList())
@@ -45,7 +44,5 @@ public abstract class EntityMixin
 				box = new Box(Math.min(box.minX, passenger.getBoundingBox().minX), box.minY, Math.min(box.minZ, passenger.getBoundingBox().minZ), Math.max(box.maxX, passenger.getBoundingBox().maxX), Math.max(box.maxY, passenger.getBoundingBox().maxY), Math.max(box.maxZ, passenger.getBoundingBox().maxZ));
 			}
 		}
-
-		return box;
 	}
 }
