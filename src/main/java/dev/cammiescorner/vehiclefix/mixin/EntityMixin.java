7601e8217c68e47da5209b804c292d5ffc6398d6
CammiePone <inazuma261@gmail.com>
2022-02-16 05:12:01 -0500

    vehiclefix 3.0 moment

--- src/main/java/dev/cammiescorner/vehiclefix/mixin/EntityMixin.java
+++ src/main/java/dev/cammiescorner/vehiclefix/mixin/EntityMixin.java
@@ -1,38 +1,46 @@
 package dev.cammiescorner.vehiclefix.mixin;
 
-import net.fabricmc.fabric.api.tag.TagFactory;
-import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.tag.Tag;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Box;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 
 @Mixin(Entity.class)
 public abstract class EntityMixin {
-	@Shadow @Final private EntityType<?> type;
-	@Shadow public abstract boolean hasPassengers();
 	@Shadow public abstract List<Entity> getPassengerList();
+	@Shadow @Nullable public abstract Entity getVehicle();
 
 	@ModifyVariable(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At(value = "INVOKE_ASSIGN",
 			target = "Lnet/minecraft/entity/Entity;getBoundingBox()Lnet/minecraft/util/math/Box;"
 	))
 	private Box getBoundingBox(Box box) {
-		final Tag<EntityType<?>> affects = TagFactory.ENTITY_TYPE.create(new Identifier("vehiclefix", "fixed_collision"));
-
-		if(hasPassengers() && (affects.values().isEmpty() || affects.contains(type)))
-			for(Entity passenger : getPassengerList())
-				box = new Box(Math.min(box.minX, passenger.getBoundingBox().minX), box.minY, Math.min(box.minZ, passenger.getBoundingBox().minZ), Math.max(box.maxX, passenger.getBoundingBox().maxX), Math.max(box.maxY, passenger.getBoundingBox().maxY), Math.max(box.maxZ, passenger.getBoundingBox().maxZ));
+		if(getVehicle() != null)
+			return new Box(box.minX, getVehicle().getBoundingBox().minY + 0.1, box.minZ, box.maxX, box.maxY, box.maxZ);
 
 		return box;
+	}
+
+	@Inject(method = "adjustMovementForCollisions(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;", at = @At(value = "RETURN",
+			ordinal = 1
+	), cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT)
+	public void vehiclefix$handleRiderCollisions(Vec3d movement, CallbackInfoReturnable<Vec3d> info, Box box, List<VoxelShape> list, Vec3d vec3d) {
+		for(Entity passenger : getPassengerList()) {
+			Vec3d passengerCollision = passenger.adjustMovementForCollisions(movement);
+
+			info.setReturnValue(new Vec3d(
+					vec3d.getX() > 0 ? Math.min(passengerCollision.getX(), vec3d.getX()) : vec3d.getX() < 0 ? Math.max(passengerCollision.getX(), vec3d.getX()) : vec3d.getX(),
+					vec3d.getY() > 0 ? Math.min(passengerCollision.getY(), vec3d.getY()) : Math.max(passengerCollision.getY(), vec3d.getY()),
+					vec3d.getZ() > 0 ? Math.min(passengerCollision.getZ(), vec3d.getZ()) : vec3d.getZ() < 0 ? Math.max(passengerCollision.getZ(), vec3d.getZ()) : vec3d.getZ()
+			));
+		}
 	}
 }
