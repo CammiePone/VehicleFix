f198f50433a3ae936a75a6e88106f0b4a86320bb
CammiePone <inazuma261@gmail.com>
2023-03-03 10:52:11 -0500

    update to 1.19.3

--- src/main/java/dev/cammiescorner/vehiclefix/BlockTags.java
+++ src/main/java/dev/cammiescorner/vehiclefix/BlockTags.java
@@ -1,10 +1,10 @@
 package dev.cammiescorner.vehiclefix;
 
 import net.minecraft.block.Block;
-import net.minecraft.tag.TagKey;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class BlockTags {
-	public static final TagKey<Block> PASSABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier("vehiclefix", "passable"));
+	public static final TagKey<Block> PASSABLE = TagKey.of(Registries.BLOCK.getKey(), new Identifier("vehiclefix", "passable"));
 }
--- src/main/java/dev/cammiescorner/vehiclefix/EntityTags.java
+++ src/main/java/dev/cammiescorner/vehiclefix/EntityTags.java
@@ -1,18 +1,18 @@
 package dev.cammiescorner.vehiclefix;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.tag.TagKey;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntryList;
 
 import java.util.Optional;
 
 public class EntityTags {
-	public static final TagKey<EntityType<?>> AFFECTS = TagKey.of(Registry.ENTITY_TYPE_KEY, new Identifier("vehiclefix", "affects"));
+	public static final TagKey<EntityType<?>> AFFECTS = TagKey.of(Registries.ENTITY_TYPE.getKey(), new Identifier("vehiclefix", "affects"));
 
 	public static boolean affectsIsEmpty() {
-		Optional<RegistryEntryList.Named<EntityType<?>>> tag = Registry.ENTITY_TYPE.getEntryList(EntityTags.AFFECTS);
+		Optional<RegistryEntryList.Named<EntityType<?>>> tag = Registries.ENTITY_TYPE.getEntryList(EntityTags.AFFECTS);
 
 		return tag.isEmpty() || tag.get().size() == 0;
 	}
